c81369f71b35e6cfffa4f8e10f2a4b3bcb4dd52e
MatthewMarinets <matthew.marinets@gmail.com>
2026-01-28 11:19:03 -0800

    sc2: Updating download progress bar * Use pipes and spaces * Printing MiB amounts * Using colours

--- worlds/sc2/client.py
+++ worlds/sc2/client.py
@@ -101,37 +101,6 @@ TRADE_LOCK_WAIT_LIMIT = 540000 / 1.4 # Time in ms that the client may spend tryi
 STARCRAFT2 = "Starcraft 2"
 STARCRAFT2_WOL = "Starcraft 2 Wings of Liberty"
 
-# Text helpers
-CHAR_WIDTHS: dict[str, float] = {
-    ",": 0.8,
-    "i": 0.9, "j": 0.9, "l": 0.9,
-    "!": 0.95,
-    "I": 1.0, ".": 1.0,
-    "t": 1.15,
-    "r": 1.25, "f": 1.25,
-    "v": 1.75, "y": 1.75,
-    "x": 1.8, "z": 1.75,
-    "c": 1.85, "k": 1.85, "s": 1.85, "<": 1.85, ">": 1.85,
-    "a": 1.95, "e": 1.95, "L": 1.95,
-    "F": 1.975,
-    "h": 2.0, "u": 2.0,
-    "p": 2.02,
-    "o": 2.05, "q": 2.05, "g": 2.05, "d": 2.05, "b": 2.05, "E": 2.05, "J": 2.05,
-    "$": 2.0,
-    "S": 2.15, "Z": 2.15,
-    "T": 2.2, "Y": 2.2, "#": 2.2,
-    "B": 2.25, "K": 2.25, "R": 2.25, "X": 2.25,
-    "P": 2.3,
-    "C": 2.35, "D": 2.35, "U": 2.35, "V": 2.35,
-    "A": 2.4,
-    "G": 2.45, "O": 2.45, "Q": 2.45,
-    "H": 2.55, "N": 2.55,
-    "w": 2.75,
-    "m": 3.1,
-    "M": 3.15, "W": 3.15,
-}
-"""Approximate character widths in Roboto Medium, in periods"""
-
 # Data version file path.
 # This file is used to tell if the downloaded data are outdated
 # Associated with /download_data command
@@ -576,9 +545,9 @@ class StarcraftClientProcessor(ClientCommandProcessor):
 
         tempzip, metadata = download_latest_release_zip(
             DATA_REPO_OWNER, DATA_REPO_NAME, DATA_API_VERSION,
+            ctx,
             metadata=metadata,
             force_download=True,
-            monospace_progress=ctx.ui is None,
         )
 
         if tempzip:
@@ -2309,9 +2278,9 @@ def download_latest_release_zip(
     owner: str,
     repo: str,
     api_version: str,
+    ctx: SC2Context,
     metadata: str | None = None,
     force_download: bool = False,
-    monospace_progress: bool = False,
 ) -> tuple[str, str | None]:
     """Downloads the latest release of a GitHub repo to the current directory as a .zip file."""
     import requests
@@ -2320,17 +2289,10 @@ def download_latest_release_zip(
     url = f"https://api.github.com/repos/{owner}/{repo}/releases/tags/{api_version}"
     user_facing_url = f"https://github.com/{owner}/{repo}/releases/tag/{api_version}"
 
-    CHUNK_SIZE = 1 << 22  # 4 MiB
-    LOADED_MESSAGE = "Downloading"
-    PROGRESS_FRACTION = len(LOADED_MESSAGE)
-    DEFAULT_CHAR_WIDTH = 2.0
-    if monospace_progress:
-        def dot_width(string: str) -> int:
-            return len(string)
-    else:
-        def dot_width(string: str) -> int:
-            return int(sum(CHAR_WIDTHS.get(c, DEFAULT_CHAR_WIDTH) for c in string) + 0.5)
-    NUM_DOTS = dot_width(LOADED_MESSAGE)
+    MB_SCALE = 20
+    CHUNK_SCALE = 22  # 4 MiB
+    CHUNK_SIZE = 1 << CHUNK_SCALE
+    PROGRESS_FRACTION = 10
 
     r2: requests.Response | None = None
     try:
@@ -2359,6 +2321,15 @@ def download_latest_release_zip(
             r2.close()
             sc2_logger.warning(f"Download failed with status code {r2.status_code}.")
             return "", metadata
+
+        def print_progress_bar(progress: int, mb_written: int, mb_total: int) -> None:
+            # In Roboto, `|` and ` ` are very similar widths, so this displays nicely everywhere
+            (
+                ColouredMessage('Progress: [ ')
+                .coloured("|||" * progress, colour="orange")
+                (("   " * (PROGRESS_FRACTION - progress)) + f" ] {mb_written} / {mb_total} MiB")
+            ).send(ctx)
+
         with open(file, "wb") as fp:
             total_length = r2.headers.get("content-length")
             if total_length is None:
@@ -2366,15 +2337,14 @@ def download_latest_release_zip(
             else:
                 bytes_written = 0
                 bytes_total = int(total_length)
+                total_length_megabytes = bytes_total >> MB_SCALE
                 last_progress = -1
                 for chunk in r2.iter_content(chunk_size=CHUNK_SIZE):
                     bytes_written += len(chunk)
                     fp.write(chunk)
                     progress = bytes_written * PROGRESS_FRACTION // bytes_total
                     if progress > last_progress:
-                        loaded_string = LOADED_MESSAGE[:progress]
-                        num_dots = NUM_DOTS - dot_width(loaded_string)
-                        sc2_logger.info(f"Progress: [ {loaded_string}{'.'*num_dots} ]")
+                        print_progress_bar(progress, bytes_written >> MB_SCALE, total_length_megabytes)
                         last_progress = progress
                 if bytes_written < bytes_total:
                     sc2_logger.warning(
