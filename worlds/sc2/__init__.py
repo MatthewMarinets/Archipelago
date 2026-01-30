053cc4d96ea86a0a3f3075467443438cdfdc319c
MatthewMarinets <matthew.marinets@gmail.com>
2026-01-30 13:17:24 -0800

    sc2: Fixing victory modulo usage in init.py

--- worlds/sc2/__init__.py
+++ worlds/sc2/__init__.py
@@ -263,7 +263,8 @@ class SC2World(World):
                     if lookup_location_id_to_type[location.address] == LocationType.VICTORY_CACHE:
                         # Ensure that if there are multiple items given for finishing a mission and that at least
                         # one is progressive, the flag kept is progressive.
-                        location_name = self.location_id_to_name[(location.address // VICTORY_MODULO) * VICTORY_MODULO]
+                        location_id = (location.address // locations.VICTORY_MODULO) * locations.VICTORY_MODULO
+                        location_name = self.location_id_to_name[location_id]
                         old_classification = mission_item_classification.get(location_name, 0)
                         mission_item_classification[location_name] = old_classification | location.item.classification.as_flag()
                     else:
