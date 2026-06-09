40e1537af8a07437829dd7c20c9cb476abd1578d
MatthewMarinets <matthew.marinets@gmail.com>
2026-06-09 00:21:59 -0700

    sc2: Purge import typing from item subpackage

--- worlds/sc2/item/item_groups.py
+++ worlds/sc2/item/item_groups.py
@@ -1,4 +1,3 @@
-import typing
 from . import item_tables, item_names
 from .item_tables import key_item_table
 from ..mission_tables import campaign_mission_table, SC2Campaign, SC2Mission, SC2Race
@@ -20,12 +19,14 @@ For non-developers the following will be useful:
 * Hand-crafted item groups can be found at the bottom of this file
 """
 
-item_name_groups: typing.Dict[str, typing.List[str]] = {}
+item_name_groups: dict[str, list[str]] = {}
 
 # Groups for use in world logic
 item_name_groups["Missions"] = ["Beat " + mission.mission_name for mission in SC2Mission]
-item_name_groups["WoL Missions"] = ["Beat " + mission.mission_name for mission in campaign_mission_table[SC2Campaign.WOL]] + \
-                                   ["Beat " + mission.mission_name for mission in campaign_mission_table[SC2Campaign.PROPHECY]]
+item_name_groups["WoL Missions"] = (
+    ["Beat " + mission.mission_name for mission in campaign_mission_table[SC2Campaign.WOL]]
+    + ["Beat " + mission.mission_name for mission in campaign_mission_table[SC2Campaign.PROPHECY]]
+)
 
 # These item name groups should not show up in documentation
 unlisted_item_name_groups = {
@@ -41,7 +42,7 @@ unlisted_item_name_groups = {
 
 # Some item names only differ in bracketed parts
 # These items are ambiguous for short-hand name groups
-bracketless_duplicates: typing.Set[str]
+bracketless_duplicates: set[str]
 # This is a list of names in ItemNames with bracketed parts removed, for internal use
 _shortened_names = [(name[:name.find(' (')] if '(' in name else name)
                     for name in [item_names.__dict__[name] for name in item_names.__dir__() if not name.startswith('_')]]
@@ -218,7 +219,7 @@ class ItemGroupNames:
     KEYS = "Keys"
 
     @classmethod
-    def get_all_group_names(cls) -> typing.Set[str]:
+    def get_all_group_names(cls) -> set[str]:
         return {
             name for identifier, name in cls.__dict__.items()
             if not identifier.startswith('_')
